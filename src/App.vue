8e0c3914f3ea4d2804872f416d79f309d4348de2
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-01 18:41:32 -0600

    seperated code into templates for cleaner App and add styling

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <template>
-    <ul>
-        <learning-resource
-            v-for="res in storedResources"
-            :key="res.id"
-            :title="res.title"
-            :description="res.description"
-            :link="res.link"
-        >
-        </learning-resource>
-    </ul>
+    <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-import LearningResource from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
 
 export default {
     components: {
-        'learning-resource': LearningResource,
+        'stored-resources': StoredResources,
     },
     data() {
         return {
@@ -38,3 +29,19 @@ export default {
     }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+    box-sizing: border-box;
+}
+
+html {
+    font-family: 'Roboto', sans-serif;
+}
+
+body {
+    margin: 0;
+}
+</style>
---
+++ src/components/learning-resources/StoredResources.vue
@@ -0,0 +1,38 @@
+<template>
+    <ul>
+        <learning-resource
+            v-for="res in resources"
+            :key="res.id"
+            :title="res.title"
+            :description="res.description"
+            :link="res.link"
+        >
+        </learning-resource>
+    </ul>
+</template>
+
+<script>
+import LearningResource from './LearningResource.vue';
+
+export default {
+    components: {
+        'learning-resource': LearningResource,
+    },
+    props: {
+        resources: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    margin: auto;
+    max-width: 40rem;
+}
+</style>
