89f69a3ab6e7efc8885c08ffd5b87fa7da735e51
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-01 21:11:59 -0600

    add BaseButton component for styling buttons

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import { createApp } from 'vue';
 
 import App from './App.vue';
 import BaseCard from './components/ui/BaseCard.vue';
+import BaseButton from './components/ui/BaseButton.vue';
 
 const app = createApp(App);
 
 app.component('base-card', BaseCard);
+app.component('base-button', BaseButton);
 
 app.mount('#app');
