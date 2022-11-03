5ef3abead0c4f736422b431a97e596d621754cb9
Alfredo3232 <61888719+Alfredo3232@users.noreply.github.com>
2022-11-02 19:12:56 -0600

    add error handling when user submits any empty inputs

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,4 +1,13 @@
 <template>
+    <base-dialog v-if="inputIsInvalid" title="Invalid Input" @close="confirmError">
+        <template #default>
+            <p>Unfortunately, at least one input value is invalid.</p>
+            <p>Please check all inputs and make sure you enter at least a few characters into each input field.</p>
+        </template>
+        <template #actions>
+            <base-button @click="confirmError">Okay</base-button>
+        </template>
+    </base-dialog>
     <base-card>
         <form @submit.prevent="submitData">
             <div class="form-control">
@@ -26,19 +35,30 @@
 <script>
 export default {
     inject: ['addResource'],
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
     methods: {
         submitData() {
             const enteredTitle = this.$refs.titleInput.value;
             const enteredDescription = this.$refs.descInput.value;
             const enteredUrl = this.$refs.linkInput.value;
 
-            if (enteredTitle.trim() === '' || enteredDescription.trim() === '.' || enteredUrl === '') {
-
-
+            if (
+                enteredTitle.trim() === '' ||
+                enteredDescription.trim() === '.' ||
+                enteredUrl.trim() === ''
+            ) {
+                this.inputIsInvalid = true;
                 return;
             }
 
             this.addResource(enteredTitle, enteredDescription, enteredUrl);
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
         }
     }
 }
